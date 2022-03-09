242bb80c9ac46acfdc576312d2d79fec45d92d3e
anjanisree18 <98154139+anjanisree18@users.noreply.github.com>
2022-03-09 23:02:38 +0530

    Add files via upload

---
+++ withoutarg.js
@@ -0,0 +1,12 @@
+class employee{
+    name = "anjani";
+    // constructor(name){
+    //     this.name=name;
+    // }
+    display(){
+        console.log("employee name:"+this.name);
+    }
+}
+
+let emp = new employee();
+emp.display();
